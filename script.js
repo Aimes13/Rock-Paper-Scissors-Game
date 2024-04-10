c0547170674094dc0722c15ac9dbe020d74746dc
Anne-Marie Makombe <annemariemakombe@gmail.com>
2024-04-10 17:13:26 +0200

    Add new function
    Create playerSelection funtion in script.js

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-const computerPlay = {
-  symbol: ['Rock', 'Paper', 'Scissors'],
-  computerChoice () {
-    return this.symbol[Math.floor(Math.random() * this.symbol.length)];
-  }
+function computerPlay() {
+  const symbol = ['Rock', 'Paper', 'Scissors'];
+  return symbol[Math.floor(Math.random() * symbol.length)];
 };
 
-console.log(computerPlay.computerChoice());
+console.log(computerPlay());
+
+function playerSelection() {
+    let userInput = prompt('Please enter your choice: Rock, Paper or Scissors?');
+    const userChoice = userInput.toLowerCase();
+  
+    switch (userChoice) {
+      case 'rock':
+        return `You have chosen ${userChoice}`;
+        break;
+      case 'paper':
+        return `You have chosen ${userChoice}`;
+        break;
+      case 'scissors':
+        return `You have chosen ${userChoice}`;
+        break;
+      default:
+        return this.playerSelection();
+    }
+  };
+
+console.log(playerSelection());
+/*
+function playRound(playerSelection, computerSelection) {
+
+};
+
+const playRound = 'Rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection)); */
