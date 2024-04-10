e11ae90ac526c8c4fe774ad7b73428cfb8812922
Anne-Marie Makombe <annemariemakombe@gmail.com>
2024-04-10 15:04:08 +0200

    Create function in script.js
    Create computerPlay function

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log("Hello");
+const computerPlay = {
+  symbol: ['Rock', 'Paper', 'Scissors'],
+  computerChoice () {
+    return this.symbol[Math.floor(Math.random() * this.symbol.length)];
+  }
+};
 
+console.log(computerPlay.computerChoice());
