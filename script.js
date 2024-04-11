e28908ae968e726a24e66817a01eb346ad04473b
Anne-Marie Makombe <annemariemakombe@gmail.com>
2024-04-11 16:51:57 +0200

    Update script.js
    Refactor code

--- script.js
+++ script.js
@@ -29,53 +29,34 @@ function userPlay() {
 function playRound(playerSelection, computerSelection) {
   let winner;
   if (playerSelection === computerSelection) {
-    winner = null;
     console.log( `You chose ${playerSelection} and I chose ${computerSelection} so... its a tie!`);
+    console.log(`Your current score is: ${playerScore}. My current score is: ${computerScore}.`);
   } else if (
     (playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
-    winner = true;
-    console.log( `You chose ${playerSelection} and I chose ${computerSelection} so... congratulations human, you beat me.`);
+      console.log( `You chose ${playerSelection} and I chose ${computerSelection} so... congratulations human, you beat me.`);
+      playerScore++;
+      console.log(`You have won ${playerScore} game(s).`);
+      console.log(`Your current score is: ${playerScore}. My current score is: ${computerScore}.`);
   } else {
-    winner = false;
     console.log( `You chose ${playerSelection} and I chose ${computerSelection} so... try again!`);
+    computerScore++;
+    console.log(`Your current score is: ${playerScore}. My current score is: ${computerScore}.`);
   };
-  return winner;
 };
 
 let playerSelection = userPlay();
 const computerSelection = computerPlay();
 //console.log(playRound(playerSelection, computerSelection));
-
-
 let playerScore = 0;
 let computerScore = 0;
 
-function checkWinner() {  
-  let round = playRound(playerSelection, computerSelection);
-  
-  switch (round) {
-    case true:
-      playerScore++;
-      console.log(`You have won ${playerScore} game(s).`);
-      console.log(`Your current score is: ${playerScore}. My current score is: ${computerScore}.`);
-      break;
-  case false:
-    computerScore++;
-    console.log(`Your current score is: ${playerScore}. My current score is: ${computerScore}.`);
-    break;
-  default:
-    console.log(`Your current score is: ${playerScore}. My current score is: ${computerScore}.`);
-  }
-};
-
-//checkWinner();
 function game() {
   for (let i = 0; i < 5; i++) {
     console.log(`Round ${i+1}:`);
-    checkWinner();
+    playRound(playerSelection, computerSelection);
     playerSelection = prompt(`Welcome to Round: ${i+1}`);
   }
 } 
